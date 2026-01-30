00b6c09fdcb66a1f4caba0e8131748e784760d6f
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-01-30 17:41:04 +0000

    app.js: Imported path and set up assetsPath, views path and view engine

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require("node:path");
 const app = express();
 
 const indexRouter = require("./routes/indexRouter");
 
 app.use("/", indexRouter);
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
 
 const PORT = 3000;
 
