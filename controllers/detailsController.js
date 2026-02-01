0c5d4692b7bc64605dd92f470c16a305081e46d6
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-01 21:55:44 +0000

    detailsController.js: Imported CustomNotFoundError and used in getMessageById

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -1,13 +1,13 @@
 const messages = require("../routes/indexRouter");
-// const CustomNotFoundError = require ("../errors/CustomNotFoundError");
+const CustomNotFoundError = require ("../errors/CustomNotFoundError");
 
 const getMessageById = async (req, res) => {
     const { messageId } = req.params;
     const message = await messages.getMessageById(Number(messageId));
 
-    // if (!message) {
-    //     throw new CustomNotFoundError("Author not found");
-    // }
+    if (!message) {
+        throw new CustomNotFoundError("Message not found");
+    }
 
     res.send(`Message writer: ${message.user}, Message: ${message.text}, Date: ${message.added}`);
 };
