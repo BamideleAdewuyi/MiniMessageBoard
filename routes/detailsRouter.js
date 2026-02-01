7908f2b3a7c73841e681dae3a46237c17f6280c1
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-01 16:42:32 +0000

    detailsRouter.js: Imported getMessageById from indexRouter and set detailsRouter.get to /:messageId

--- routes/detailsRouter.js
+++ routes/detailsRouter.js
@@ -1,9 +1,12 @@
 const Router = require('express');
+const { getMessageById } = require("./indexRouter")
 
 const detailsRouter = Router();
 
 detailsRouter.get("/", (req, res) => {
     res.send("DETAILS");
 });
+
+detailsRouter.get("/:messageId", getMessageById);
 
 module.exports = detailsRouter;
