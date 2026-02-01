96c29bf81e01d8cb9e221cea2b4bafac785b30e5
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-01 01:57:43 +0000

    indexRouter.js: Each message in messages has id property

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,12 +6,14 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
+    id: 1
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date(),
+    id: 2
   }
 ];
 
@@ -21,7 +23,7 @@ indexRouter.get("/", (req, res) => {
 
 indexRouter.post("/new", (req, res) => {
   const response = req.body;
-  messages.push({ text: response.message, user: response.name, added: new Date() });
+  messages.push({ text: response.message, user: response.name, added: new Date(), id: messages.length+1 });
   res.redirect("/");
 });
 
