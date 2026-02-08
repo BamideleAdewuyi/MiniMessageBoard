fdbfc13c3740eeb693ebaa26e8dac7bfb475d34b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-08 12:00:33 +0000

    detailsRouter.js: Removed first detailsRouter.get as it was redundant

--- routes/detailsRouter.js
+++ routes/detailsRouter.js
@@ -3,10 +3,6 @@ const { getMessageById } = require("../controllers/detailsController");
 
 const detailsRouter = Router();
 
-detailsRouter.get("/", (req, res) => {
-    res.send("DETAILS");
-});
-
 detailsRouter.get("/:messageId", getMessageById);
 
 module.exports = detailsRouter;
