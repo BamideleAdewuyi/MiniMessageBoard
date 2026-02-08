283b275d0ae2ec01ea88090f35c79b0797242930
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-08 17:24:30 +0000

    messagesController.js: Imported queries as db. Wrote messagesGet async function

---
+++ controllers/messagesController.js
@@ -0,0 +1,9 @@
+const db = require("../db/queries");
+
+async function messagesGet(req, res) {
+  const messages = await db.getAllMessages();
+  res.render("index", {
+        title: "Messages",
+        messages: messages,
+  });
+};
